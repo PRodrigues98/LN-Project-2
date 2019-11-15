3e127c68bd9d880eaf0b6033ef7fded856d30fd0
Pedro Rodrigues <pedro.p.rodrigues@tecnico.ulisboa.pt>
2019-11-15 00:20:22 +0000

    Cleaned up for delivery

--- delivery/run.sh
+++ delivery/run.sh
@@ -38,3 +38,29 @@ fstdraw --isymbols=syms.txt --osymbols=syms.txt --portrait FINALtransducers/lemm
 # Compila e gera a versão gráfica de word2lemma
 fstinvert FINALtransducers/lemma2word.fst > FINALtransducers/word2lemma.fst
 fstdraw --isymbols=syms.txt --osymbols=syms.txt --portrait FINALtransducers/word2lemma.fst | dot -Tpdf > FINALpdf/word2lemma.pdf
+
+tests_lemma2verb=(1 2 3 4)
+tests_lemma2word=(5 6 7 8)
+tests_word2lemma=(9 10 11 12)
+
+
+for t in ${tests_lemma2verb[@]}
+do
+	fstcompile --isymbols=syms.txt --osymbols=syms.txt  "test_$t.txt" | fstarcsort > "FINALexamples/test$t.fst"
+	fstcompose "FINALexamples/test$t.fst" "FINALtransducers/lemma2verb.fst" > "FINALexamples/test$t""_lemma2verb.fst"
+	fstdraw --isymbols=syms.txt --osymbols=syms.txt --portrait "FINALexamples/test$t""_lemma2verb.fst" | dot -Tpdf > "FINALexamples/test$t""_lemma2verb.pdf"
+done
+
+for t in ${tests_lemma2word[@]}
+do
+	fstcompile --isymbols=syms.txt --osymbols=syms.txt  "test_$t.txt" | fstarcsort > "FINALexamples/test$t.fst"
+	fstcompose "FINALexamples/test$t.fst" "FINALtransducers/lemma2word.fst" > "FINALexamples/test$t""_lemma2word.fst"
+	fstdraw --isymbols=syms.txt --osymbols=syms.txt --portrait "FINALexamples/test$t""_lemma2word.fst" | dot -Tpdf > "FINALexamples/test$t""_lemma2word.pdf"
+done
+
+for t in ${tests_word2lemma[@]}
+do
+	fstcompile --isymbols=syms.txt --osymbols=syms.txt  "test_$t.txt" | fstarcsort > "FINALexamples/test$t.fst"
+	fstcompose "FINALexamples/test$t.fst" "FINALtransducers/word2lemma.fst" > "FINALexamples/test$t""_word2lemma.fst"
+	fstdraw --isymbols=syms.txt --osymbols=syms.txt --portrait "FINALexamples/test$t""_word2lemma.fst" | dot -Tpdf > "FINALexamples/test$t""_word2lemma.pdf"
+done
